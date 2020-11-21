1c6f2855fd0d89d4f29a9140dc76bbd6aecd702f
ormiati <attila.ormos@gmail.com>
2020-11-22 00:17:46 +0100

    félkész

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+'use strict'
+
+let modal = document.getElementById("Modal");
+
+let btn = document.getElementById("Btn");
+
+let span = document.getElementsByClassName("close")[0];
+
+let btngreen = document.getElementsByClassName("btn-green")[0];
+
+let btnred = document.getElementsByClassName("btn-red")[0];
+ 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+btngreen.onclick = function() {
+    modal.style.display = 'none';
+}
+
+btnred.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
